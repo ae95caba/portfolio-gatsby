49acac2795515740bebc85c87f57066c2705cd9d
ae95caba <ae95caba@gmail.com>
2024-11-09 01:37:19 -0300

    feat: :mag: add proper metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: "André Espinoza | Desarrollador Fullstack",
+    description: "Desarrollador Fullstack Argentino adicto al mate.",
+    author: "André Espinoza",
+    siteUrl: "https://andreespinozadev.netlify.app/",
   },
   plugins: [
     "gatsby-plugin-typescript",
